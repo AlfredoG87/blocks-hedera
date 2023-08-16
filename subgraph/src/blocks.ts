edd18b0e7f106660bed5777153ec8441a3a7f647
Alfredo Gutierrez <alfredo@swirldslabs.com>
2023-08-16 15:40:12 -0600

    more changes

--- subgraph/src/blocks.ts
+++ subgraph/src/blocks.ts
@@ -2,21 +2,21 @@ import { ethereum } from "@graphprotocol/graph-ts"
 import { HederaBlock } from "../generated/schema"
 
 export function handleBlock(block: ethereum.Block): void {
-    let id = block.hash.toHex()
+    let id = block.hash.toHexString()
     let blockEntity = new HederaBlock(id);
     blockEntity.number = block.number;
     blockEntity.timestamp = block.timestamp;
-    blockEntity.parentHash = block.parentHash.toHex();
-    blockEntity.author = block.author.toHex();
+    blockEntity.parentHash = block.parentHash.toHexString();
+    blockEntity.author = block.author.toHexString();
     blockEntity.difficulty = block.difficulty;
     blockEntity.totalDifficulty = block.totalDifficulty;
     blockEntity.gasUsed = block.gasUsed;
     blockEntity.gasLimit = block.gasLimit;
-    blockEntity.receiptsRoot = block.receiptsRoot.toHex();
-    blockEntity.transactionsRoot = block.transactionsRoot.toHex();
-    blockEntity.stateRoot = block.stateRoot.toHex();
+    blockEntity.receiptsRoot = block.receiptsRoot.toHexString();
+    blockEntity.transactionsRoot = block.transactionsRoot.toHexString();
+    blockEntity.stateRoot = block.stateRoot.toHexString();
     blockEntity.size = block.size;
-    blockEntity.unclesHash = block.unclesHash.toHex();
+    blockEntity.unclesHash = block.unclesHash.toHexString();
 
     blockEntity.save();
   }
